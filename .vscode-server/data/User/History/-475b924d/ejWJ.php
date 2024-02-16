aa273d7085b74620e67c65491cc5b23658d47e0f
shrutiprakasha03 <149817702+shrutiprakasha03@users.noreply.github.com>
2024-02-16 21:08:55 +0530

    Delete .vscode-server directory

--- .vscode-server/data/User/History/-475b924d/ejWJ.php
+++
@@ -1,6 +0,0 @@
-<x-layouts.main pageName='All Songs'>
-@section('content')
-<x-songs-list :songs="$songs">
-</x-songs-list>
-@endsection
-</x-layouts.main>
--- .vscode-server/data/User/History/123a36e5/Sm9p.php
+++
@@ -1,8 +0,0 @@
-<x-layouts.main pageName="Playlist - {{$playlistId}}">
-
-@section('content')
-<x-songs-list :songs="$songs">
-</x-songs-list>
-@endsection
-
-</x-layouts.main>
